83e1d3a51e9294ee169325d6644aac4663b357ac
Kevin Shen <kevinshen34@gmail.com>
2014-04-19 15:45:31 -0700

    not every word counts

--- parser.py
+++ parser.py
@@ -1,17 +1,29 @@
-import sys
+import sys, re
+from collections import Counter
 
-def parser(file, f_name):
-	line_count = 0
-	word_count = 0
-	char_count = 0
+def parser(file, f_name, ignore):
+	line_count, word_count, char_count = 0, 0 , 0
+	nword_count, nchar_count, difference = 0, 0, 0
+	cnt = Counter()
 	for line in file:
+		char_count += len(list(line))
 		line_count += 1
 		word_count += len(line.split())
-		char_count += len(list(line))
-	print str(line_count) + " " + str(word_count) + " " + str(char_count) + " " + f_name
+		for word in filter(None, re.split(('\W+'), line)):
+			if word.lower() not in ignore:
+				nword_count += 1
+			else:
+				difference += len(word)
+				#cnt[word.lower()] += 1
+	nchar_count = char_count - difference
+	#print cnt
+	print "all: " + str(line_count) + " " + str(word_count) + " " + str(char_count) + " " + f_name
+	print "proper: " + str(line_count) + " " + str(nword_count) + " " + str(nchar_count)
 
 
 if __name__ == '__main__':
 	f_name = str(sys.argv[1])
 	f = open(f_name, 'r')
-	parser(f, f_name)
+	#ignore = ["with"]
+	ignore = ["i", "we", "you", "they", "a", "and", "the", "that", "of", "for", "with"]
+	parser(f, f_name, ignore)
